f4660552ada11ea89899bf8c2055253ebe0deed6
Ronan Vasconcelos <47048794+RonanVasconcelos@users.noreply.github.com>
2019-03-05 20:15:37 -0800

    Update Driver.java

--- Driver.java
+++ Driver.java
@@ -4,37 +4,32 @@ import java.util.ArrayList;
 
 public class Driver {
     public static void main(String[] args) {
+        ChessPiece king = new King();
+        ChessPiece queen = new Queen();
+        ChessPiece knight = new Knight();
+        ChessPiece bishop = new Bishop(); 
+        ChessPiece rook = new Rook();
+        ChessPiece pawn = new Pawn();        
 
-        //1. Initialize 6 ChessPieces
-        ChessPiece cp1 = new Bishop();
-        ChessPiece cp2 = new King();
-        ChessPiece cp3 = new Knight();
-        ChessPiece cp4 = new Pawn();
-        ChessPiece cp5 = new Queen();
-        ChessPiece cp6 = new Rook();
-
-        //2. Add all pieces to ArrayList
         ArrayList<ChessPiece> pieces = new ArrayList<>();
-        pieces.add(cp1);
-        pieces.add(cp2);
-        pieces.add(cp3);
-        pieces.add(cp4);
-        pieces.add(cp5);
-        pieces.add(cp6);
+        pieces.add(king);
+        pieces.add(queen);
+        pieces.add(knight);
+        pieces.add(bishop);
+        pieces.add(rook);
+        pieces.add(pawn);
 
-        //3. Test move() method on every piece.
         for(ChessPiece piece: pieces){
             System.out.println(piece + ": ");
             piece.move();
         }
 
-        //4. Test Pawn promotion
-        ((Pawn) cp4).promote(cp2); // cannot be promoted
+        ((Pawn) pawn).promote(rook); 
 
         Pawn newPawn = new Pawn();
-        System.out.println(newPawn.equals(cp4)); // still the same Pawn
+        System.out.println(newPawn.equals(rook)); 
 
-        ((Pawn) cp4).promote(cp3); // can be promoted to
-        System.out.println(newPawn.equals(cp4)); //should
+        ((Pawn) bishop).promote(rook); 
+        System.out.println(newPawn.equals(queen));
     }
 }
